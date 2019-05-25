58fc6a000b450d00cf73e336e418e3f3130640ea
Thomas Chartron <thomas.chartron@gmail.com>
2019-05-25 23:05:24 +0200

    :bento: Addng bulma ss framework

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+require('./styles/main.scss'); //Main css importing bulma
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
--- vue.config.js
+++ vue.config.js
@@ -9,5 +9,12 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? `${process.cwd()}/dist/`
-    : '/'
+    : '/',
+    // css: {
+    // loaderOptions: {
+    //     sass: {
+    //             data: `@import "@/styles/_variables.scss";`
+    //         }
+    //     }
+    // }
 }
