50cf7823b2d95f4f39448bf0787c54e39c59d778
Thomas Chartron <thomas.chartron@gmail.com>
2019-05-26 21:00:05 +0200

    :bug: Allowing Cross Origin Resource Share

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const { app, BrowserWindow } = require('electron')
 let win
 
 function createWindow () {
-    win = new BrowserWindow({ width: 800, height: 600 })
+    win = new BrowserWindow({ width: 800, height: 600, webPreferences: {webSecurity: false}})
 
     if(process.env.NODE_ENV === 'development') {
         // console.log('devv')
