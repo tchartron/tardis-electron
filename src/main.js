e18a1d59ae7635337902f7f91fd994413e5a51f8
Thomas Chartron <thomas.chartron@gmail.com>
2019-06-04 23:29:52 +0200

    :memo: Explanations and allowing use of require('electron') in renderer by telling webpack explicitly (target) in vue.config.js

--- src/main.js
+++ src/main.js
@@ -2,8 +2,14 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import App from './App.vue'
 import routes from './services/routes'
-// const electron = require('electron')
-const electron = require('electron')
+const electron = require('electron') // see vue.config.js
+// const electron = window.require('electron')
+//why window.require ?
+/*
+WIW, I ran into the same issue trying to use electron in the renderer process with create-react-app that uses webpack in the backend instead of browserify. window.require seems to solve it for me too though I'm not entirely clear why.
+
+Edit: I figured out why :-) We want to require electron during runtime from the nodejs environment provided at the runtime rather than the nodejs environment used during compilation by webpack. By default, globals are bound to window and webpack compilation ignores the window global - hence window.require works.
+ */
 
 Vue.prototype.$electron = electron;
 
