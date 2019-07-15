7e675fc27f02f049b2e2ad503ec5a3fe625d20e2
Thomas Chartron <thomas.chartron@gmail.com>
2019-07-15 16:07:52 +0200

    :sparkles: :art: Automatically starting and stoping timer based on filesystem actions and user idle time is working so well

--- src/components/Watch.vue
+++ src/components/Watch.vue
@@ -43,7 +43,7 @@
                             <div class="field">
                                 <div class="control">
                                     <div class="select is-fullwidth">
-                                        <select name="tasks" @change="getTaskDetails" v-model="selectedTask">
+                                        <select name="tasks" @change="getTaskDetails(true)" v-model="selectedTask">
                                             <option value="0">Pick a task</option>
                                             <option v-for="task in tasks" :value="task.id">{{ task.title }}</option>
                                         </select>
@@ -69,14 +69,17 @@
                 <div class="column">
                     <div class="box" v-if="taskSummary != null">
                         <p class="underline">Task summary :</p>
-                        <p class="">Total time spent {{ taskSummary.totalTime }}</p>
-                        <p class="">Your time spent {{ taskSummary.userTime }}</p>
-                        <p class="">Total timers {{ taskSummary.timersNumber }}</p>
+                        <p class="">Total time spent <span class="bold">{{ taskSummary.totalTime }}</span></p>
+                        <p class="">Your time spent<span class="bold"> {{ taskSummary.userTime }}</span></p>
+                        <p class="">Total timers <span class="bold">{{ taskSummary.timersNumber }}</span></p>
+                        <button class="button is-warning is-small has-margin-top-10" @click="getTaskDetails(false)">
+                            Refresh
+                        </button>
                     </div>
                 </div>
             </div>
             <div class="control has-icons-left has-icons-right">
-                <input class="input is-medium is-rounded" type="text" placeholder="/path/to/watch" v-model="pathToWatch">
+                <input class="input is-medium is-rounded" type="text" placeholder="/path/to/watch" v-model="pathToWatch" :disabled="(selectedTask === 0)">
                 <span class="icon is-left">
                     <i class="fas fa-folder-open"></i>
                 </span>
@@ -138,7 +141,10 @@ export default {
             loadingMessage: String,
             isWatching: false,
             taskSummary: null,
-            currentTimer: null,
+            currentTimer: {
+                path: '',
+                timer: {}
+            },
             idleInterval: null,
             idleTime: 0
         }
@@ -156,7 +162,8 @@ export default {
         stopWatch() {
             this.isWatching = !this.isWatching;
             this.$electron.ipcRenderer.send('stop')
-            this.clearIdleInterval();
+            // this.pathToWatch = "";
+            this.clearIdleInterval(true);
             // console.log('stop sent')
         },
         findGroups() {
@@ -186,7 +193,7 @@ export default {
                 this.isLoading = false;
             });
         },
-        getTaskDetails() {
+        getTaskDetails(details) {
             this.isLoading = true
             this.loadingMessage = "Getting task details ...";
             let backend = new BackendService();
@@ -195,7 +202,9 @@ export default {
                 this.taskDetails = response.data
                 // this.isLoading = false;
                 this.taskStatistics();
-                this.showTaskDetails = true;
+                if(details !== false) {
+                    this.showTaskDetails = true;
+                }
             }, (error) => {
                 console.log(error)
                 this.isLoading = false;
@@ -203,6 +212,9 @@ export default {
         },
         taskStatistics() {
             this.loadingMessage = "Fetching task and user stats ...";
+            if(this.isLoading !== true) {
+                this.isLoading = true;
+            }
             let totalTime = 0
             let userTime = 0
             if(this.taskDetails.timers.length > 0) {
@@ -236,50 +248,76 @@ export default {
         },
         startTimer() {
             let backend = new BackendService();
+            //Attach path about to be watched to timer creation
+            this.currentTimer.path = this.pathToWatch;
             backend.storeTimer(this.api.access_token, this.selectedGroup, this.selectedTask)
             .then((response) => {
-                this.currentTimer = response.data.timer;
+                this.currentTimer.timer = response.data.timer;
+                console.log('start timer'+this.currentTimer);
                 toast({
                     message: "Timer started go to work",
                     type: "is-success",
                     dismissible: true,
+                    position: 'bottom-right',
                     animate: { in: "fadeIn", out: "fadeOut" }
                 });
+                console.log('start toast should be displayed')
             }, (error) => {
                 console.log(error)
+                //reset path to watch attached to timer upon timer creation failure
+                this.currentTimer.path = "";
+                this.currentTimer.timer = {};
             })
         },
         stopTimer() {
             let backend = new BackendService();
-            backend.updateTimer(this.api.access_token, this.selectedGroup, this.selectedTask, this.currentTimer.id)
+            //reset path beeing watched
+            // this.currentTimer.path = "";
+            backend.updateTimer(this.api.access_token, this.selectedGroup, this.selectedTask, this.currentTimer.timer.id)
             .then((response) => {
-                this.currentTimer = null;
+                //Also in the view
+                // this.pathToWatch = "";
+                this.currentTimer.timer = {};
+                console.log('stop timer'+this.currentTimer)
                 toast({
                     message: "Timer stoped go to rest",
                     type: "is-warning",
                     dismissible: true,
                     animate: { in: "fadeIn", out: "fadeOut" }
                 });
+                console.log('stop toast should be displayed')
             }, (error) => {
                 console.log(error)
+                //cancel what has been done before send request because request failed and so
+                // this.currentTimer.path = this.pathToWatch;
+
+                //Recursive call to stopTimer() to make this timer stop .... ?
             })
         },
         countIdle() {
             this.idleInterval = setInterval(() => {
                 this.idleTime++;
                 if(this.idleTime > 30) {
-                    this.clearIdleInterval();
+                    this.clearIdleInterval(false);
                 }
             }, 1000)
         },
-        clearIdleInterval() {
+        clearIdleInterval(stopped) {
             if(this.idleInterval) {
                 clearInterval(this.idleInterval);
                 this.idleTime = 0;
             }
-            if(this.currentTimer =! null) {
-                this.stopTimer();
+            if(stopped === true) {
+                //Reset both path to watch and path beeing watched
+                this.pathToWatch = "";
+                this.currentTimer.path = "";
+            } else {
+                //not explicitly stopped we just have oveflowed idleTime just stop timer keep on watching
+                this.currentTimer.path = "";
             }
+            // if(this.currentTimer.path === this.pathToWatch) {
+            this.stopTimer();
+            // }
         }
     },
     mounted() {
@@ -287,15 +325,16 @@ export default {
             this.watcherData.push(data)
             //reset idleTime something just happened
             this.idleTime = 0;
-            if(this.currentTimer === null) {
-                this.currentTimer = true; //to avoid creating thousand timers
+
+            //If timer already attached to path do nothing
+            if(this.currentTimer.path !== this.pathToWatch) {
                 this.startTimer();
             }
+
         });
         //Populates the groups select
         this.findGroups();
     }
-
 };
 </script>
 
