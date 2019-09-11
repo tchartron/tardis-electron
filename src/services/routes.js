4ff394f4636db460340eea9f6ffec05f44f5b328
Thomas Chartron <thomas.chartron@gmail.com>
2019-09-11 12:50:11 +0200

    :wrench: Changed routing

--- src/services/routes.js
+++ src/services/routes.js
@@ -2,6 +2,8 @@ import App from './../App.vue'
 import Login from './../components/Login.vue'
 import Watch from './../components/Watch.vue'
 import Clock from './../components/Clock.vue'
+import TimerPage from './../components/pages/TimerPage.vue'
+import SettingPage from './../components/pages/SettingPage.vue'
 
 export default {
     mode: 'history',
@@ -14,15 +16,27 @@ export default {
             path: '/login',
             name: 'login',
             component: Login
-        }, {
-            path: '/watch',
-            name: 'watch',
-            component: Watch,
+        },
+        // , {
+        //     path: '/watch',
+        //     name: 'watch',
+        //     component: Watch,
+        //     props: true
+        // }, {
+        //     path: '/clock',
+        //     name: 'clock',
+        //     component: Clock,
+        //     props: true
+        // }
+        {
+            path: '/timer-page',
+            name: 'timer-page',
+            component: TimerPage,
             props: true
         }, {
-            path: '/clock',
-            name: 'clock',
-            component: Clock,
+            path: '/setting-page',
+            name: 'setting-page',
+            component: SettingPage,
             props: true
         }
     ]
