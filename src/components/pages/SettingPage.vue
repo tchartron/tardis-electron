8e817235e5834553750746100828eed99a438b78
Thomas Chartron <thomas.chartron@gmail.com>
2019-09-19 16:16:43 +0200

    :fire: Cleaning comments and saveSetting method begining

--- src/components/pages/SettingPage.vue
+++ src/components/pages/SettingPage.vue
@@ -49,7 +49,7 @@
                             </div>
                             <div class="field">
                                 <p class="control has-text-centered">
-                                    <button class="button is-info">
+                                    <button class="button is-info" @click="saveSettings">
                                         Save
                                     </button>
                                 </p>
@@ -74,69 +74,12 @@
 import Header from '@/components/layout/Header.vue';
 import Footer from '@/components/layout/Footer.vue';
 import BulmaswatchThemeService from '@/services/bulmaswatch-themes-service'
-// const ThemeHelper = function() {
-
-//   const preloadTheme = (href) => {
-//     let link = document.createElement('link');
-//     link.rel = "stylesheet";
-//     link.href = href;
-//     document.head.appendChild(link);
-
-//     return new Promise((resolve, reject) => {
-//       link.onload = e => {
-//         const sheet = e.target.sheet;
-//         sheet.disabled = true;
-//         resolve(sheet);
-//       };
-//       link.onerror = reject;
-//     });
-//   };
-
-//   const selectTheme = (themes, name) => {
-//     if (name && !themes[name]) {
-//       throw new Error(`"${name}" has not been defined as a theme.`);
-//     }
-//     Object.keys(themes).forEach(n => themes[n].disabled = (n !== name));
-//   }
-
-//   let themes = {};
-
-//   return {
-//     add(name, href) { return preloadTheme(href).then(s => themes[name] = s) },
-//     set theme(name) { selectTheme(themes, name) },
-//   };
-// };
 
 export default {
     data() {
         return {
-            themes: {
-                    // cosmo: "https://unpkg.com/bulmaswatch@0.7.5/cosmo/bulmaswatch.min.css",
-                    // cerulean: "https://unpkg.com/bulmaswatch@0.7.5/cerulean/bulmaswatch.min.css",
-                    // cyborg: "https://unpkg.com/bulmaswatch@0.7.5/cyborg/bulmaswatch.min.css",
-                    // darkly: "https://unpkg.com/bulmaswatch@0.7.5/darkly/bulmaswatch.min.css",
-                    // default: "https://unpkg.com/bulmaswatch@0.7.5/default/bulmaswatch.min.css",
-                    // flatly: "https://unpkg.com/bulmaswatch@0.7.5/flatly/bulmaswatch.min.css",
-                    // journal: "https://unpkg.com/bulmaswatch@0.7.5/journal/bulmaswatch.min.css",
-                    // litera: "https://unpkg.com/bulmaswatch@0.7.5/litera/bulmaswatch.min.css",
-                    // lumen: "https://unpkg.com/bulmaswatch@0.7.5/lumen/bulmaswatch.min.css",
-                    // lux: "https://unpkg.com/bulmaswatch@0.7.5/lux/bulmaswatch.min.css",
-                    // materia: "https://unpkg.com/bulmaswatch@0.7.5/materia/bulmaswatch.min.css",
-                    // minty: "https://unpkg.com/bulmaswatch@0.7.5/minty/bulmaswatch.min.css",
-                    // nuclear: "https://unpkg.com/bulmaswatch@0.7.5/nuclear/bulmaswatch.min.css",
-                    // pulse: "https://unpkg.com/bulmaswatch@0.7.5/pulse/bulmaswatch.min.css",
-                    // sandstone: "https://unpkg.com/bulmaswatch@0.7.5/sandstone/bulmaswatch.min.css",
-                    // simplex: "https://unpkg.com/bulmaswatch@0.7.5/simplex/bulmaswatch.min.css",
-                    // slate: "https://unpkg.com/bulmaswatch@0.7.5/slate/bulmaswatch.min.css",
-                    // solar: "https://unpkg.com/bulmaswatch@0.7.5/solar/bulmaswatch.min.css",
-                    // spacelab: "https://unpkg.com/bulmaswatch@0.7.5/spacelab/bulmaswatch.min.css",
-                    // superhero: "https://unpkg.com/bulmaswatch@0.7.5/superhero/bulmaswatch.min.css",
-                    // united: "https://unpkg.com/bulmaswatch@0.7.5/united/bulmaswatch.min.css",
-                    // yeti: "https://unpkg.com/bulmaswatch@0.7.5/yeti/bulmaswatch.min.css"
-            },
+            themes: {},
             themesVersion: ""
-            // themeHelper: new ThemeHelper(),
-            // loading: true
         }
     },
     components: {
@@ -146,12 +89,12 @@ export default {
     methods: {
         navigate(dest) {
             this.$router.push({ name: dest})
+        },
+        saveSettings() {
+
         }
     },
     computed: {
-        // loadingMessage() {
-        //     return this.$store.state.loadingMessage
-        // }
         user() {
             return this.$store.state.user
         },
@@ -176,16 +119,6 @@ export default {
         }
     },
     created() {
-        // add/load themes
-        // let added = Object.keys(this.themes).map(name => {
-        //     return this.themeHelper.add(name, this.themes[name]);
-        // });
-
-        // Promise.all(added).then(sheets => {
-        //     console.log(`${sheets.length} themes loaded`);
-        //     this.loading = false;
-        //     this.themeHelper.theme = "flatly";
-        // });
     },
     mounted() {
         let themesService = new BulmaswatchThemeService()
