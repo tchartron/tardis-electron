4896ef1ee03d35deadb6406f91d64ec821b56d31
Thomas Chartron <thomas.chartron@gmail.com>
2019-09-26 15:22:52 +0200

    :art: :bug: Trying to implement 401 reponse interceptor for every axios requests

--- src/services/backend-service.js
+++ src/services/backend-service.js
@@ -4,6 +4,7 @@ import axios from 'axios'
 const apiAdress = "http://127.0.0.1:8181";
 const apiSuffix = "/api";
 const apiUrl = apiAdress + apiSuffix;
+const _401interceptor = null;
     // var headers = {
     //     'Access-Control-Allow-Origin': '*',
     //     'Content-Type': 'application/json'
@@ -12,6 +13,46 @@ import store from '@/store/store'
 import differenceInSeconds from 'date-fns/differenceInSeconds'
 
 export default class BackendService {
+    constructor() {
+        this.mount401Interceptor();
+    }
+
+    mount401Interceptor() {
+        this._401interceptor = axios.interceptors.response.use(
+            (response) => {
+                return response
+            },
+            async (error) => {
+                if (error.request.status == 401) {
+                    console.log("refreshing token")
+                    // Refresh the access token
+                    try{
+                        let url = `${apiUrl}/login`;
+                        await axios.post(url, {
+                            email: user.email,
+                            password: user.password
+                        });
+                        console.log("retrying original request")
+                        // Retry the original request
+                        return axios({
+                            method: error.config.method,
+                            url: error.config.url,
+                            data: error.config.data
+                        })
+                        // return axios(error.config)
+                    } catch (e) {
+                        // Refresh has failed - reject the original request
+                        throw error
+                    }
+                }
+                // If error was not 401 just reject as is
+                throw error
+            }
+        )
+    }
+    unmount401Interceptor() {
+        axios.interceptors.response.eject(this._401interceptor)
+    }
 
     async renewToken(user, api) {
         let now = new Date(Date.now());
@@ -40,35 +81,35 @@ export default class BackendService {
         });
     }
     async getGroups(api) {
-        await this.renewToken(store.getters.user, store.getters.api)
+        // await this.renewToken(store.getters.user, store.getters.api)
         let url = `${apiUrl}/groups`;
         return axios.get(url, {
             headers: {"Authorization": `Bearer ${api.access_token}`}
         })
     }
     async getTasks(api, groupId) {
-        await this.renewToken(store.getters.user, store.getters.api)
+        // await this.renewToken(store.getters.user, store.getters.api)
         let url = `${apiUrl}/groups/${groupId}/tasks`;
         return axios.get(url, {
             headers: {"Authorization": `Bearer ${api.access_token}`}
         })
     }
     async getTask(api, groupId, taskId) {
-        await this.renewToken(store.getters.user, store.getters.api)
+        // await this.renewToken(store.getters.user, store.getters.api)
         let url = `${apiUrl}/groups/${groupId}/tasks/${taskId}`;
         return axios.get(url, {
             headers: {"Authorization": `Bearer ${api.access_token}`}
         })
     }
     async storeTimer(api, groupId, taskId) {
-        await this.renewToken(store.getters.user, store.getters.api)
+        // await this.renewToken(store.getters.user, store.getters.api)
         let url = `${apiUrl}/groups/${groupId}/tasks/${taskId}/timers`;
         return axios.post(url, {}, {
             headers: {"Authorization": `Bearer ${api.access_token}`}
         })
     }
     async updateTimer(api, groupId, taskId, timerId) {
-        await this.renewToken(store.getters.user, store.getters.api)
+        // await this.renewToken(store.getters.user, store.getters.api)
         let url = `${apiUrl}/groups/${groupId}/tasks/${taskId}/timers/${timerId}`;
         return axios.put(url, {}, {
             headers: {"Authorization": `Bearer ${api.access_token}`}
